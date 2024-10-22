cd10a0cef938f603469aae7d6360bc6ed121b87d
CsehiTamas <csehit@kkszki.hu>
2024-10-22 10:32:09 +0200

    third commit

---
+++ Data/BlogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Dolgozat_gyakorló_II_.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dolgozat_gyakorló_II_.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options) { }
+
+        public DbSet<BlogUser> blogUsers { get; set; }
+    }
+}
